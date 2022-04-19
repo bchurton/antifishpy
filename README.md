c9c113565333889ab47fbfc7499d574f183368d7
vierofernando <vierofernando9@gmail.com>
2022-04-19 10:12:56 +0700

    refactor: more refactor!!!!

--- README.md
+++ README.md
@@ -8,33 +8,27 @@ Use the package manager [pip](https://pypi.org/project/antifishpy/) to install t
 pip install antifishpy
 ```
 
-## Examples
-
+## Example
 ```python
 from antifishpy import antifish
-from discord.ext import commands
+from collections import namedtuple
+import asyncio
 
-client = commands.Bot(command_prefix="!")
-af = antifish("Your Bot Name | Application Link") # This is to pass your application name as the User-Agent header.
-
-@client.event
-async def on_message(message):
-    msg = await af.check_message(message)
-
-    # expect True or False.
-    print(msg.match)
-
-    # expect True or False. this returns True if the trust_rating threshold is >= 0.95.
-    print(msg.is_scam)
-
-    # the serialized first element of the "matches" array.
-    print(msg.matches)
-
-# use this when the bot is about to exit! (highly recommended)
-async def some_exit_function():
+async def main():
+    af = antifish("amogus application")
+    
+    # check a scam URL like the one below;
+    res = await af.check_message("https://discord-nitro.com")
+    
+    print(res.match, res.is_scam)
+    
+    for m in res.matches:
+        print(m.domain, m.url, m.trust_rating, m.is_scam)
+    
     await af.close()
 
-client.run("TOKEN")
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 ```
 
 ## Credit where it's due
--- antifishpy.py
+++ antifishpy.py
@@ -1,11 +1,22 @@
 from aiohttp import ClientSession
 from collections import namedtuple
+from enum import Enum
 import re
+
+class DomainType:
+    PHISHING = 0
+    IP_LOGGER = 1
 
 INITIAL_MATCH_REGEX = re.compile("(?:[A-z0-9](?:[A-z0-9-]{0,61}[A-z0-9])?\.)+[A-z0-9][A-z0-9-]{0,61}[A-z0-9]")
 
-Matches = namedtuple("Matches", "followed domain source type trust_rating")
+Matches = namedtuple("Matches", "followed domain source type trust_rating url is_scam")
 AntiFishMatches = namedtuple("AntiFishMatches", "match matches is_scam")
+
+def create_matches(data: dict) -> "Matches":
+    data["type"] = DomainType.PHISHING if data["type"] == "PHISHING" else DomainType.IP_LOGGER
+    data["is_scam"] = data["trust_rating"] >= 0.95
+    
+    return Matches(**data)
 
 class antifish:
     __slots__ = ("session",)
@@ -15,19 +26,20 @@ class antifish:
             "User-Agent": f"{application} - via Antifish-py module"
         })
     
-    async def check_message(self, message):
+    def __repr__(self) -> str:
+        return f"Antifish(\"{self.session.headers['User-Agent'][:-25]}\")"
+    
+    async def check_message(self, content: str) -> "AntiFishMatches":
         """ Checks the message. """
         
-        if not INITIAL_MATCH_REGEX.search(message.content):
-            return Matches(False, None, False)
+        if not INITIAL_MATCH_REGEX.search(content):
+            return AntiFishMatches(False, None, False)
 
-        async with self.session.get("https://anti-fish.bitflow.dev/check", json={ "message": message.content }) as response:
+        async with self.session.get("https://anti-fish.bitflow.dev/check", json={ "message": content }) as response:
             json = await response.json()
             
-            matches = json.get("matches", [ None ])[0]
-            is_scam = matches is not None and json["match"] and matches["trust_rating"] >= 0.95
-            
-            return AntiFishMatches(json["match"], Matches(**matches) if matches is not None else None, is_scam)
+            matches = [create_matches(m) for m in json.get("matches", [])]
+            return AntiFishMatches(json["match"], matches or None, any(m.trust_rating >= 0.95 for m in matches))
     
     async def close(self):
         """ Closes the antifish instance. """
